1c9f12a640ef9a70f4784fca35dd929e489b7017
VasileiosOik <econom07@hotmail.com>
2018-07-11 16:27:53 +0100

    Refactoring the RabbitMq

--- src/main/java/com/example/messagehandlers/MessageCreator.java
+++ src/main/java/com/example/messagehandlers/MessageCreator.java
@@ -1,13 +1,9 @@
 package com.example.messagehandlers;
 
+import com.example.domain.CustomMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
-import com.example.domain.CustomMessage;
-import com.example.domain.Header;
-import com.example.domain.Payload;
-import com.example.domain.Person;
 
 @Component
 public class MessageCreator {
@@ -25,36 +21,8 @@ public class MessageCreator {
 	}
 
 	public CustomMessage addMessage(CustomMessage message) {
-	//	CustomMessage customMessage = constructNewPersonDetailsMessage();
 		LOG.debug("Inside the SendingMessageProcessor.class---->The message added to the queue is: [{}]",
 				message);
 		return message;
 	}
-
-//	private CustomMessage constructNewPersonDetailsMessage() {
-//		CustomMessage message = new CustomMessage();
-//		message.setHeader(getHeader());
-//		message.setPayload(getPayload());
-//		return message;
-//	}
-//
-//	private Payload getPayload() {
-//		return new Payload(getPerson());
-//
-//	}
-//
-//	private Person getPerson() {
-//		Person person=new Person();
-//		person.setAge(29);
-//		person.setName("Bill");
-//		return person;
-//	}
-//
-//	private Header getHeader() {
-//		Header header=new Header();
-//		header.setMessageId("2");
-//		header.setEvent("as.request.received");
-//		return header;
-//	}
-
 }
