e18c8e95633a73bb305ceeedd2608250a46ab21a
bill <econom07@hotmail.com>
2019-03-25 19:46:30 +0000

    general fixes

--- src/main/java/com/connection/configuration/RabbitMqConsumerConfig.java
+++ src/main/java/com/connection/configuration/RabbitMqConsumerConfig.java
@@ -12,13 +12,13 @@ import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Component
+@Configuration
 @PropertySource("classpath:common.properties")
 public class RabbitMqConsumerConfig {
 
@@ -29,7 +29,7 @@ public class RabbitMqConsumerConfig {
     @Value("${ext.received.event}")
     private List<String> bindingKeys;
 
-    @Value("${*.queue.name}")
+    @Value("${company.queue.name}")
     private String nameQueue;
 
     @Autowired
